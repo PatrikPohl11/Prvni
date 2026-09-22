c5306efd16d93b7bb9f1e729857acb6d683eb2c2
PatrikPohl11 <pohlpatrikk@gmail.com>
2026-09-22 19:56:42 +0200

    Skript

---
+++ Dalsiskript.py
@@ -0,0 +1,9 @@
+#ngfdjfgbjd
+cele_c = 50
+real_c = 50.5
+pi_c = 3.14
+binarni_cislo = 0b101010
+hexadecimalni_cislo = 0xFF
+
+print ("cele cislo", cele_c)
+print ("hexadecimalni cislo", hexadecimalni_cislo)
